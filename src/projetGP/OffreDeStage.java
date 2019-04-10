ef3843540065fe9a6780b64b89d3aae33437708b
Ludovic CHAULIAC <lchauliac@a101-18.iut.univ-paris8.fr>
2019-04-10 16:20:23 +0200

    modif2

--- src/projetGP/OffreDeStage.java
+++ src/projetGP/OffreDeStage.java
@@ -1,14 +1,6 @@
 package projetGP;
 
-public class OffreDeStage {
-	private String descriptif;
-	private int id;
-	
-	public OffreDeStage(int newID) {
-		this.id = newID;
-		this.descriptif = "test projet";
-	}
-	
+//yeah
 	
 
-}
+
